a9232b4d334ed4af7c8feac33bc069f68c360820
Jagriti <jagriti.agni@gmail.com>
2020-11-11 00:08:55 +0530

    [Jagriti]ADDED: UC5-While Loop to find wage for a month

--- NewEmployeeWage.js
+++ NewEmployeeWage.js
@@ -44,3 +44,18 @@ for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
 }
 empWage = empHrs * WAGE_PER_HOUR;
 console.log("Total Hrs: " + empHrs + " employee wage " + empWage);
+
+// UC 5 while loop
+{
+    const MAX_HRS_IN_MONTH = 100;
+    let totalEmpHrs = 0;
+    let totalWorkingDays = 0;
+    while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+        totalWorkingDays++;
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        totalEmpHrs += getWorkingHours(empCheck);
+    }
+    empWage = totalEmpHrs * WAGE_PER_HOUR;
+    console.log("UC5- Total Days: " + totalWorkingDays +
+        " Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
+}
