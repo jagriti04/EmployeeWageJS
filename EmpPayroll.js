a4c77861fa23f6215e5b54b60973997b7a06de1a
Jagriti <jagriti.agni@gmail.com>
2020-11-17 01:40:10 +0530

    [Jagriti]ADDED: Try catch and check if salary, id, gender are valid

--- EmpPayroll.js
+++ EmpPayroll.js
@@ -17,6 +17,28 @@ class EmployeePayrollData {
         else throw 'Name is Incorrect!';
     }
 
+    get id() { return this._id; }
+    set id(id) {
+        let idRegex = RegExp('^[1-9]\d*$');
+        if (idRegex.test(id))
+            this._id = id;
+        else throw 'Id is Incorrect!';
+    }
+
+    get salary() { return this._salary; }
+    set salary(salary) {
+        if (salary > 0)
+            this._salary = salary;
+        else throw 'Salary is Incorrect!';
+    }
+
+    get gender() { return this._gender; }
+    set gender(gender) {
+        if (gender === undefined || gender == "m" || gender == "M" || gender == "F" || gender == "f")
+            this._gender = gender;
+        else throw 'gender is invalid';
+    }
+
     // method
     toString() {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
@@ -35,5 +57,21 @@ try {
 } catch (e) {
     console.error(e);
 }
-let newEmployeePayrollData = new EmployeePayrollData(1, "Terrisa", 30000, "F", new Date());
-console.log(newEmployeePayrollData.toString());
+try {
+    let newEmployeePayrollData = new EmployeePayrollData(1, "Terrisa", 30000, "F", new Date());
+    console.log(newEmployeePayrollData.toString());
+} catch (e1) {
+    console.error(e1);
+}
+try {
+    let newEmployeePayrollData2 = new EmployeePayrollData(2, "Jeff", -30000, "M", new Date());
+    console.log(newEmployeePayrollData2.toString());
+} catch (e2) {
+    console.error(e2);
+}
+try {
+    let newEmployeePayrollData3 = new EmployeePayrollData(1, "Bill", 40000, "b", new Date());
+    console.log(newEmployeePayrollData3.toString());
+} catch (e3) {
+    console.error(e3);
+}
