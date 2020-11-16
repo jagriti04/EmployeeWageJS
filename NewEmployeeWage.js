79f14e419961a0e0b44b973ea9e8e141dcf0fcc6
Jagriti <jagriti.agni@gmail.com>
2020-11-16 22:53:49 +0530

    [Jagriti]ADDED: Create object and show daily hours and wage

--- NewEmployeeWage.js
+++ NewEmployeeWage.js
@@ -69,8 +69,8 @@ function totalWages(totalWage, dailyWage) {
     return totalWage + dailyWage;
 }
 
+const MAX_HRS_IN_MONTH = 160;
 {
-    const MAX_HRS_IN_MONTH = 160;
     let totalEmpHrs = 0;
     let totalWorkingDays = 0;
     let empDailyWageArr = new Array();
@@ -116,3 +116,28 @@ function totalWages(totalWage, dailyWage) {
     console.log("Part Working Days: " + partWorkingDays);
     console.log("Non Working Days: " + nonWorkingDays);
 }
+
+// UC 10 Object Creation
+{
+    let totalEmpHrs = 0;
+    let totalWorkingDays = 0;
+    let empDailyHrsAndWageArr = new Array();
+    while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+        totalWorkingDays++;
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        let empHrs = getWorkingHours(empCheck);
+        totalEmpHrs += empHrs;
+        empDailyHrsAndWageArr.push(
+            {
+                dayNum: totalWorkingDays,
+                dailyHours: empHrs,
+                dailyWage: calcDailyWage(empHrs),
+                toString() {
+                    return '\nDay' + this.dayNum + ' => Working Hours is ' + this.dailyHours + ' And Wage Earned = '
+                        + this.dailyWage
+                },
+            });
+    }
+    console.log("UC10 Showing Daily Hours Worked and Wage Earned: " + empDailyHrsAndWageArr);
+}
+
