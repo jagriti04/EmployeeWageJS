18bee2297b98ef64243c2ca520d4b813274e3616
Jagriti <jagriti.agni@gmail.com>
2020-11-17 00:37:17 +0530

    [Jagriti]REFACTORED:In Employee payroll class store gender and start date

--- EmpPayroll.js
+++ EmpPayroll.js
@@ -1,9 +1,11 @@
 class EmployeePayrollData {
     // constructor
-    constructor(id, name, salary) {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+    constructor(...params) {
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
 
     // getter and setter method
@@ -12,7 +14,11 @@ class EmployeePayrollData {
 
     // method
     toString() {
-        return "id=" + this.id + ", name='" + this.name + "' , salary=" + this.salary;
+        const options = {year: 'numeric', month: 'long', day: 'numeric'};
+        const empDate = this.startDate === undefined ? "undefined" :
+                        new Date(this.startDate).toLocaleDateString("en-US", options);
+        return "id=" + this.id + ", name='" + this.name + "' , salary=" + this.salary
+                + ", gender=" + this.gender + ", startDate=" + empDate;
     }
 }
 
@@ -20,3 +26,5 @@ let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000);
 console.log(employeePayrollData.toString());
 employeePayrollData.name = "john";
 console.log(employeePayrollData.toString());
+let newEmployeePayrollData = new EmployeePayrollData(1, "Terrisa", 30000, "F", new Date());
+console.log(newEmployeePayrollData.toString());
